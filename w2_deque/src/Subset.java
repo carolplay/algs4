aa3edef425ace19fe96db88e00f4dc753ee0ab98
carolplay <polyguoyang@gmail.com>
2016-10-15 02:29:12 +0200

    w2 subset

--- w2_deque/src/Subset.java
+++ w2_deque/src/Subset.java
@@ -1,6 +1,22 @@
+import java.util.Iterator;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 
 public class Subset {
 	public static void main(String[] args) {
+		RandomizedQueue<String> rq = new RandomizedQueue<String>();
+		int k = Integer.parseInt(args[0]);
+		while (!StdIn.isEmpty()) {
+			String item = StdIn.readString();
+			rq.enqueue(item);
+		}
+		
+		Iterator<String> it = rq.iterator();
+		for (int i = 0; i < k; i++) {
+			StdOut.println(it.next());
+		}
 		
 	}
 }
